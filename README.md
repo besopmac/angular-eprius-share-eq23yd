3b512fc52753df12890573ffbbd55733ba55055e
besopmac <brunosopmac@gmail.com>
2021-02-26 12:22:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-eprius-share-mornh5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-eprius-share-mornh5)
---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-data-table",
+  templateUrl: "./data-table.component.html",
+  styleUrls: ["./data-table.component.css"]
+})
+export class DataTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
